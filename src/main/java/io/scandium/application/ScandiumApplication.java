a081d9a17c1e9b306ab9a4808b465163cf78010d
lipingan <shianlee@hotmail.com>
2021-11-20 15:08:36 +0800

    [Type] Feature [Scope] application [Description]   Basic Springboot application setup.

---
+++ src/main/java/io/scandium/application/ScandiumApplication.java
@@ -0,0 +1,13 @@
+package io.scandium.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScandiumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScandiumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/scandium/application/ScandiumApplicationTests.java
@@ -0,0 +1,13 @@
+package io.scandium.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScandiumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
